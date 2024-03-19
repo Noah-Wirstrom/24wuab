4b8818adeb17e449170e0204d0ac2a07db69b5a7
Noah-Wirstrom <noah.wirstrom@elev.ga.ntig.se>
2024-03-19 19:22:48 +0100

    Did it all
    hela sidan fullt fungereande för såväl mobil som dator

--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
 
+    document.getElementById('scrollToAbout').addEventListener('click', function() {
+    
+        document.getElementById('about').scrollIntoView({ 
+            behavior: 'smooth' 
+        });
+    });
+
+    document.getElementById('scrollToArbeten').addEventListener('click', function() {
+    
+        document.getElementById('work').scrollIntoView({ 
+            behavior: 'smooth' 
+        });
+    });
+
+    document.getElementById('scrollToUtbildning').addEventListener('click', function() {
+    
+        document.getElementById('education').scrollIntoView({ 
+            behavior: 'smooth' 
+        });
+    });
+
+
+});
+
+
+
+    
    
